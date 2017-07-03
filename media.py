2a9a193cb450b5dab06c06b84cd5282797a09227
Brian Kim <briankim1512@gmail.com>
2017-07-03 22:18:16 +0900

    Improved media.py to meet PEP8 standards as well

--- media.py
+++ media.py
@@ -1,35 +1,41 @@
-#Initial import for one of the lessons for the function 'showTrailer'
+# Initial import for one of the lessons for the function 'showTrailer'
 import webbrowser
 
-#Class video allows instances to be made of a movie for __doc__.
-#I made this into a parent class as the last lesson describes using TV shows as well
+# Class video allows instances to be made of a movie for __doc__.
+# I made this into a parent class as the last lesson describes using TV too
+
+
 class Video():
-    #__doc__ contents
+    # __doc__ contents
     """This class accepts and provides video information"""
-    
-    #global variables for methods inside Video class
-    VALID_RATINGS=['G','PG','PG-13','R']
 
-    #Initializing constructor to create video instances
+    # global variables for methods inside Video class
+    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
+
+    # Initializing constructor to create video instances
     def __init__(self, title, storyline, posterImage, trailerYoutube):
-        self.title=title
-        self.storyline=storyline
-        self.poster_image_url=posterImage
-        self.trailer_youtube_url=trailerYoutube
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = posterImage
+        self.trailer_youtube_url = trailerYoutube
 
-    #Method to show trailer without the website generator
+    # Method to show trailer without the website generator
     def showTrailer(self):
         webbrowser.open(self.trailerYoutubeURL)
 
-#The children classes for the Video class. For convenience of not having to write code again.
-#Hope you don't mind reading a bit more code...
+# The children classes for the Video class. For convenience of not having
+# to write code again.
+# Hope you don't mind reading a bit more code...
+
+
 class Movie(Video):
-    #__doc__ contents
+    # __doc__ contents
     """This class accepts and provides Movie show information"""
 
-    #Initializing constructor to create movie instances
+    # Initializing constructor to create movie instances
     def __init__(self, title, storyline, posterImage, trailerYoutube):
         Video.__init__(self, title, storyline, posterImage, trailerYoutube)
+
 
 class TvShow(Video):
     """This class accepts and provides TV show information"""
